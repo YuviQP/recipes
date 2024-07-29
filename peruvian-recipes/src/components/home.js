b4e7eccf0f9f88d369f9c135d1ed9bb9a6ddc0b7
YuviQP <yuvisa.palomino@gmail.com>
2024-07-29 20:35:39 -0300

    initialize project

---
+++ peruvian-recipes/src/components/home.js
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+function Home() {
+ 
+  return (
+    <>
+      <Container>
+        <h1>Recetas</h1>
+       
+      </Container>
+    </>
+  );
+}
+
+export default Home;
